04b67fe49d9eecc76e44ede5e24120b1d13275e1
max <maxwelgithumbi@gmail.com>
2022-05-02 19:28:54 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,7 +8,7 @@ from typing import List
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "names.csv"
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
