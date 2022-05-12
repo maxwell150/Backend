74a9bcca7de1e172ecdabb77dcfa6445cf6dd1ee
max <maxwelgithumbi@gmail.com>
2022-05-12 09:56:17 +0300

    get locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Get locale for request
+"""
+
+from flask import request, Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''Babel config'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello() -> str:
+    """returns string
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
